c5db22c5d0c46ec25be20fcda187033186855774
Adam Goode <agoode@andrew.cmu.edu>
2010-06-23 14:55:27 -0400

    Add Nuclear Area Breast, javascript

---
+++ Nuclear/Nuclear_Area_Breast.js
@@ -0,0 +1,29 @@
+IJ.run("Colour Deconvolution", "vectors=H&E");
+IJ.getImage().close();
+IJ.getImage().close();
+
+var img = IJ.getImage();
+IJ.run(img, "8-bit", "");
+IJ.setAutoThreshold(img, "Default");
+IJ.run(img, "Convert to Mask", "");
+IJ.run(img, "Analyze Particles...", "size=0-Infinity circularity=0.00-1.00 show=Nothing clear include summarize record");
+
+// compute area fraction
+var w = img.getWidth();
+var h = img.getHeight();
+
+var area = w * h;
+
+var tArea = 0;
+var i;
+var rt = ResultsTable.getResultsTable();
+for (i = 0; i < rt.getCounter(); i++) {
+  tArea += rt.getValue("Area", i);
+}
+
+//print("tArea: " + tArea);
+//print("area: " + area);
+var areaFrac = (tArea / area * 100.0);   // area fraction in percent
+print("areaFrac: " + areaFrac);
+
+IJ.run("Diamond Filter", "expr=" + (areaFrac * 10000));
